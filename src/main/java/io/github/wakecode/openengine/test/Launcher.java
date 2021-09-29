eadce1124d17bb621004a779f958263b761699c3
wake-code <87005812+wake-code@users.noreply.github.com>
2021-09-29 22:57:13 +0300

    Update Launcher

--- src/main/java/io/github/wakecode/openengine/test/Launcher.java
+++ src/main/java/io/github/wakecode/openengine/test/Launcher.java
@@ -1,9 +1,19 @@
 package io.github.wakecode.openengine.test;
 
+import io.github.wakecode.openengine.core.WindowManager;
 import org.lwjgl.Version;
 
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("[INFO] Using LWJGL v" + Version.getVersion());
+
+        WindowManager window = new WindowManager("OpenEngine", 1600, 900, false);
+        window.init();
+
+        while (!window.windowShouldClose()) {
+            window.update();
+        }
+
+        window.cleanup();
     }
 }
