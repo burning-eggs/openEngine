f04a2ca53aa44f07ab812f825664726dfdad3234
wake-code <87005812+wake-code@users.noreply.github.com>
2021-09-29 14:44:09 +0300

    openEngine Initial

---
+++ src/main/java/io/github/wakecode/openengine/test/Launcher.java
@@ -0,0 +1,9 @@
+package io.github.wakecode.openengine.test;
+
+import org.lwjgl.Version;
+
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("[INFO] Using LWJGL v" + Version.getVersion());
+    }
+}
