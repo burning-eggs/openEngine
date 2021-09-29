c66fa6ba6648aff0f0853c470209fe7825ddd0a4
wake-code <87005812+wake-code@users.noreply.github.com>
2021-09-29 23:33:07 +0300

    RenderManager

---
+++ src/main/java/io/github/wakecode/openengine/core/RenderManager.java
@@ -0,0 +1,28 @@
+package io.github.wakecode.openengine.core;
+
+import io.github.wakecode.openengine.test.Launcher;
+import org.lwjgl.opengl.GL11;
+
+public class RenderManager {
+    private final WindowManager window;
+
+    public RenderManager() {
+        window = Launcher.getWindow();
+    }
+
+    public void init() throws Exception {
+
+    }
+
+    public void render() {
+
+    }
+
+    public void clear() {
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+    }
+
+    public void cleanup() {
+        
+    }
+}
